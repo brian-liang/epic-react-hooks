e07bb2806fbec31b3c63024afc99164423a24c23
brian-liang <brian.j.liang@gmail.com>
2022-12-01 20:30:29 -0700

    54 - Extra credit colocate state

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,11 +3,12 @@
 
 import * as React from 'react'
 
-function Name({name, onNameChange}) {
+function Name() {
+  const [name, setName] = React.useState('')
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={onNameChange} />
+      <input id="name" value={name} onChange={e => setName(e.target.value)} />
     </div>
   )
 }
@@ -33,21 +34,20 @@ function FavoriteAnimal({animal, setAnimal}) {
 // }
 
 // 💣 remove this component in favor of the new one
-function Display({name, animal}) {
-  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+function Display({animal}) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
 }
 
 function App() {
   // 🐨 add a useState for the animal
-  const [name, setName] = React.useState('')
   const [animal, setAnimal] = React.useState('')
   return (
     <form>
-      <Name name={name} onNameChange={event => setName(event.target.value)} />
+      <Name />
       {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
       <FavoriteAnimal animal={animal} setAnimal={setAnimal} />
       {/* 🐨 pass the animal prop here */}
-      <Display name={name} animal={animal} />
+      <Display animal={animal} />
     </form>
   )
 }
